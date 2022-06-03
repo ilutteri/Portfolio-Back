ab6a017f6d28b10122f2014e8e10956613286e3a
Ignacio <ignaciolutteri@gmail.com>
2022-06-03 00:01:34 -0300

    make it better

--- src/main/java/com/ilutteri/portfolio/model/Profile.java
+++ src/main/java/com/ilutteri/portfolio/model/Profile.java
@@ -1,5 +1,7 @@
 package com.ilutteri.portfolio.model;
 
+import org.hibernate.validator.constraints.Email;
+
 import javax.persistence.*;
 import java.io.Serializable;
 
@@ -11,6 +13,8 @@ public class Profile implements Serializable {
     private Long id;
     private String name;
     private String title;
+    @Email
+    private String email;
     private String bannerUrl;
     private String profileUrl;
     private String about;
@@ -18,10 +22,11 @@ public class Profile implements Serializable {
     public Profile() {
     }
 
-    public Profile(Long id, String name, String title, String bannerURL, String profileURL, String about) {
+    public Profile(Long id, String name, String title, String email, String bannerURL, String profileURL, String about) {
         this.id = id;
         this.name = name;
         this.title = title;
+        this.email = email;
         this.bannerUrl = bannerURL;
         this.profileUrl = profileURL;
         this.about = about;
@@ -51,6 +56,10 @@ public class Profile implements Serializable {
         this.title = title;
     }
 
+    public String getEmail() { return email; }
+
+    public void setEmail(String email) { this.email = email; }
+
     public String getBannerURL() {
         return bannerUrl;
     }
@@ -75,14 +84,17 @@ public class Profile implements Serializable {
         this.about = about;
     }
 
+
+
     @Override
     public String toString() {
         return "Profile{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
                 ", title='" + title + '\'' +
-                ", bannerURL='" + bannerUrl + '\'' +
-                ", profileURL='" + profileUrl + '\'' +
+                ", email='" + email + '\'' +
+                ", bannerUrl='" + bannerUrl + '\'' +
+                ", profileUrl='" + profileUrl + '\'' +
                 ", about='" + about + '\'' +
                 '}';
     }
